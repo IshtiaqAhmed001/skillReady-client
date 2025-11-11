022d687afb523c182df4fbbac5542fd25f8e86ee
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-11-11 22:14:50 +1100

    added env variable

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,12 @@
-import React, { use } from "react";
+import React from "react";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import { useLoaderData } from "react-router";
 import PopularCourses from "../../components/PopularCourses/PopularCourses";
-import { AuthContext } from "../../contexts/AuthContext";
 
 const Home = () => {
   const courses = useLoaderData();
   const popularCourses = courses.slice(1, 7);
-  const {name} = use(AuthContext)
-  console.log(name)
+
   return (
     <div>
       <HeroSection></HeroSection>
